fd4dd91563f06756d8eb7acfb842088fed55b6f4
2donw-jpg <submarineindefense@gmail.com>
2024-10-29 16:16:16 -0600

    Cambio en diseño

--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -17,8 +17,7 @@ const PokemonList = (props: PokemonProps) =>{
                 style={styles.ImagePokemon}
                 source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/2a6a6b66983a97a6bdc889b9e0a2a42a25e2522e/sprites/pokemon/${props.codigo}.png`}}/>
                 <View style = {styles.contenedorDatosGenerales }>
-                    <Text style={styles.nombreEnFicha}>{props.pokemons.name}</Text>
-                    <Text style={styles.nombreEnFicha}>ID: {props.codigo}</Text>
+                    <Text style={styles.nombreEnFicha}>No. {props.codigo} {props.pokemons.name}</Text>
                 </View>
             </View>   
         </View>
@@ -26,38 +25,41 @@ const PokemonList = (props: PokemonProps) =>{
     );  
 };
 const styles = StyleSheet.create({
-    contenedorPokemon:{
-        height: 90,
-        backgroundColor: "#ffa400",
-        display: 'flex',
-        flexDirection: 'row',
-        marginBottom: 5,
-        padding: 5,
-        borderStyle: 'solid',
-        borderWidth: 3,
-        borderColor: '#341257',
-        borderRadius: 45,
+    contenedorPokemon: {
+      height: 90,
+      backgroundColor: "#CBCBCB", // Primary color
+      display: 'flex',
+      flexDirection: 'row',
+      marginBottom: 5,
+      padding: 5,
+      borderStyle: 'solid',
+      borderStartWidth: 7,
+      borderBottomEndRadius: 50 ,
+      borderTopEndRadius: 50,
+      borderColor: '#Bf070D', // Secondary color
     },
-    nombreEnFicha:{
-        fontSize: 24,
-        color: '#332911',
-        fontWeight: 'bold',
+    nombreEnFicha: {
+      fontSize: 24,
+      color: '#121212', // Secondary color
+      fontWeight: 'bold',
     },
     contenedorDatosGenerales: {
-        display:'flex',
-        flexDirection: 'column',
+      display: 'flex',
+      flexDirection: 'column',
+        
     },
     ImagePokemon: {
-        height: 76,
-        width: 100,
-        marginRight: 10,
+      height: 76,
+      width: 100,
+      marginRight: 10,
     },
     container: {
-        flex: 1,
-        backgroundColor: "#ffde00",
-
-      },
-});
-
+      flex: 1,
+      backgroundColor: "#f0f0f0", // Secondary color
+      paddingHorizontal: 10,
+      paddingVertical: 3
+    },
+  });
+  
 
 export default PokemonList;
